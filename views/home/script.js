31cd2c22b8fc37177aaf4794e0e60aa6454849d5
161718 <99414095+33355555@users.noreply.github.com>
2023-08-09 22:23:22 +0700

    Update script.js

--- views/home/script.js
+++ views/home/script.js
@@ -1,18 +1,25 @@
 axios({
-    method: 'post',
-    url: '/crawl'
-}).then(res => {
-    const tableListBody = document.querySelector("#listCode");
-    tableListBody.innerHTML = '';
-    res.data.forEach((item, index) => {
-        tableListBody.innerHTML += `
-            <tr>
-                <td>${index + 1}</td>
-                <td>${item.name}</td>
-                <td>${item.code}</td>
-            </tr>
-        `
-    })
-}).catch(err => {
-    console.log(err)
+  method: "post",
+  url: "/crawl",
 })
+  .then((response) => {
+    console.log(response);
+    const tableListBody = document.querySelector("#listCode");
+    tableListBody.innerHTML = "";
+
+    response.data.forEach((item, index) => {
+      Object.keys(item.codes).forEach((groupName) => {
+        tableListBody.innerHTML += `
+          <tr>
+            <td>${index + 1}</td>
+            <td>${item.mail}</td>
+            <td>${groupName}</td>
+            <td>${item.codes[groupName]}</td>
+          </tr>
+        `;
+      });
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
